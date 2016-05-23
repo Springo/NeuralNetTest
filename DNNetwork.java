3b148ed8a5a6e00537bd8bfaa9b28af4f1f24381
Springo <kevinmxia@gmail.com>
2016-05-23 16:57:46 -0700

    added neural network backbone

---
+++ DNNetwork.java
@@ -0,0 +1,30 @@
+/** 
+ *  Created by Janice Huang and Kevin Xia on May, 2016
+ */
+
+public class DNNetwork {
+	private Perceptron output;
+	private Perceptron [] inputs;
+
+	public DNNetwork(int numins) {
+		inputs = new Perceptron[numins];
+		for (int i = 0; i < inputs.length; i++) {
+			inputs[i] = new Perceptron();
+		}
+		output = new Perceptron(inputs);
+	}
+
+	//TODO: train
+
+	public double predict(double [] vals) {
+		System.out.println("Predicting...");
+		if (vals == null || vals.length != inputs.length) {
+			System.out.printf("Error: Invalid Input");
+			return 0.0;
+		}
+		for (int i = 0; i < inputs; i++) {
+			inputs[i].updateValue(vals[i]);
+		}
+		return output.getValue();
+	}
+}
